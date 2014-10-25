7a6e55f2dda0a9492b5949176328e3aa8d5dc436
Tim Reinke <tim@mostcallmetim.com>
2014-10-25 15:03:37 -0700

    Comment out stuff that doesn't actually work

--- src/lein_ui/nrepl/http.clj
+++ src/lein_ui/nrepl/http.clj
@@ -10,30 +10,38 @@
 ;; TODO figure out users
 ;; TODO register watcher for messages
 
-(defmulti handle-message (fn [event & args] event))
+(comment
+  (defmulti handle-message (fn [event & args] event))
 
-(defmethod handle-message [:ws :connect] [_ session channel])
+         (defmethod handle-message [:ws :connect] [_ session]
+           (let [user (session :user)
+                 channel (session :ws/channel)]
+             (ui-nrepl/subscribe! user
+                                  (fn [msg]
+                                    (server/send! channel {:type :nrepl/message
+                                                           :body msg})))))
 
-(defmethod handle-message [:ws :receive] [_ session msg]
-  ;; (validate-nrepl-msg! msg :or send-error :to session)
-  ;; (some-> (validate-nrepl-msg! msg) (send-error session))
-  (let [session (client-manager/load-repl-session session (:subject msg))]
-    (client-manager/send-message! session (:subject msg) (:body msg))))
+         (defmethod handle-message [:ws :receive] [_ session msg]
+           ;; (validate-nrepl-msg! msg :or send-error :to session)
+           ;; (some-> (validate-nrepl-msg! msg) (send-error session))
+           (let [session (client-manager/load-repl-session session (:subject msg))]
+             (client-manager/send-message! session (:subject msg) (:body msg))))
 
-(defmethod handle-message [:ws :close] [_ session status]
-  (println "Session" session "disconnected with" status))
+         (defmethod handle-message [:ws :close] [_ session status]
+           (println "Session" session "disconnected with" status))
 
-(defn nrepl-socket-handler [request]
-  (let [user :current-user]
-    (server/with-channel request channel
-      (server/on-receive channel (fn [msg]
-                                   (try
-                                     (handle-message msg user (request :lein-ui/app-state))
-                                     (catch Throwable t
-                                       (println t)
-                                       (server/send! channel (pprint-str
-                                                              {:type :error
-                                                               :exception (str t)}))))))
-      (server/on-close channel (fn [status]
-                                 ;; (schedule (-> 10 minutes) #(cleanup user)
-                                 (println "User" user "disconnected"))))))
+         (defn nrepl-socket-handler [session]
+           (let [user :current-user]
+             (server/with-channel request channel
+               (handle-message [:ws :connect] session)
+               (server/on-receive channel (fn [msg]
+                                            (try
+                                              (handle-message msg user (request :lein-ui/app-state))
+                                              (catch Throwable t
+                                                (println t)
+                                                (server/send! channel (pprint-str
+                                                                       {:type :error
+                                                                        :exception (str t)}))))))
+               (server/on-close channel (fn [status]
+                                          ;; (schedule (-> 10 minutes) #(cleanup user)
+                                          (println "User" user "disconnected")))))))
