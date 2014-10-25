04c2363710b71c51c55bbf3b89aca93113b3b3fa
Tim Reinke <tim@mostcallmetim.com>
2014-10-25 15:26:53 -0700

    It starts after removing multiple projects

--- src/lein_ui/core.clj
+++ src/lein_ui/core.clj
@@ -18,124 +18,37 @@
 
 ;;; Projects
 
-(defonce projects (atom {}))
+(defonce project (atom nil))
+
+(defn get-project* []
+  (if-let [project @project]
+    project
+    (throw (ex-info "Project doesn't exist" {:name name}))))
 
 (defn new-project [project-map raw-project-map]
   (assoc project-map
     ::run-state (atom {})
     ::raw raw-project-map))
 
-(defn get-project* [name]
-  (if-let [project (@projects name)]
-    project
-    (throw (ex-info "Project doesn't exist" {:name name}))))
-
 (defn load-project! [root]
   (let [path (->>  (io/file root "project.clj")
                       (.getAbsolutePath))
-        project (project/read path)
-        raw-project (project/read-raw path)
-        ui-project (new-project project raw-project)
+        project-map (project/read path)
+        raw-project-map (project/read-raw path)
+        ui-project (new-project project-map raw-project-map)
         name (-> ui-project :name)
         root (-> ui-project :root)]
-       (swap! projects (fn [ps]
-                         (when (ps name)
-                           (throw (ex-info (str "Project already loaded")
-                                           {:root root})))
-                         (assoc ps name
-                                ui-project)))
-       name))
+    (reset! project ui-project)
+    name))
 
-
-(defn unload-project! [name]
-  (swap! projects dissoc name))
-
-(defn reload-project! [name]
-  (let [root (-> (get-project* name) :root)]
-    (unload-project! name)
-    (load-project! root)))
-
-;; adapated from leiningen.core.eval
-(defn- overridden-env
-  "Returns an overridden version of the current environment as an Array of
-  Strings of the form name=val, suitable for passing to Runtime#exec."
-  [env]
-  (->> env
-       (filter val)
-       (map #(str (name (key %)) "=" (val %)))
-       (into-array String)))
-
-;; adapated from leiningen.core.eval
-(defn sh
-  [cmd & {:keys [dir]}]
-  (let [env (overridden-env (System/getenv))
-        proc (.exec (Runtime/getRuntime) (into-array cmd) env
-                    (io/file dir))]
-    (.addShutdownHook (Runtime/getRuntime)
-                      (Thread. (fn [] (.destroy proc))))
-    ;; TODO: wrap setup in try/finally and close streams
-    (let [out (io/reader (.getInputStream proc))
-          err (io/reader (.getErrorStream proc))
-          out-writer (java.io.StringWriter.)
-          err-writer (java.io.StringWriter.)
-          pump-out (doto (Pipe. out out-writer) .start)
-          pump-err (doto (Pipe. err err-writer) .start)]
-      {:process proc
-       :pump-out pump-out
-       :pump-err pump-err
-       :out-writer out-writer
-       :err-writer err-writer})))
-
-
-(defonce free-port (atom 4000))
-(defn next-free-port []
-  (swap! free-port inc))
-
-(defn start-repl! [name]
-  (let [project (get-project* name)]
-    (when (-> project ::run-state deref :repl)
-      (throw (ex-info "Project already running a repl!" {:name name})))
-
-    (let [env (overridden-env (System/getenv))
-          host (repl/repl-host project)
-          port (let [project-port (repl/repl-port project)]
-                 (if (= project-port 0)
-                   (next-free-port)
-                   project-port))
-          repl-process (sh ["lein"
-                            "repl"
-                            ":headless"
-                            ":host"
-                            host
-                            ":port"
-                            (str port)]
-                           :dir ".")]
-      (swap! (::run-state project) assoc :repl
-             (assoc repl-process
-               :host host
-               :port port))
-      nil)))
-
-
-(defn reset-repl! [name]
-  (reset! (-> name get-project* ::run-state)
-          nil))
-
-(defn stop-repl! [name]
-  (throw (ex-info "Not supported" {:error :go-away}))
-  (let [project (get-project* name)]
-    (if-let [repl (-> project ::run-state deref :repl)]
-      (do (.destroy (:process repl))
-          (swap! (::run-state project) dissoc :repl)
-          nil)
-      (throw (ex-info "repl not running" {:name name})))))
 
 (def self-repl {:host "localhost"
                 :port 7888})
 
 
-(defn repl-eval! [name code]
-  (let [{:keys [host port]} (-> name get-project* ::run-state deref :repl)]
+;; TODO remove this for nrepl.client-manager
+(defn repl-eval! [code]
+  (let [{:keys [host port]} (-> (get-project*) ::run-state deref :repl)]
     (with-open [conn (nrepl/connect :host host
                                     :port port)]
       (-> (nrepl/client conn 1000)
@@ -143,45 +56,41 @@
           doall))))
 
 
-(defn figwheel-process [project]
+(defn figwheel-process []
   (let [out-writer (java.io.StringWriter.)]
     {:thread (.start
               (Thread. (bound-fn []
                          (binding [*out* out-writer]
-                           (figwheel/figwheel project)))))
+                           (figwheel/figwheel (get-project*))))))
      :out-writer out-writer}))
 
-(defn start-figwheel! [name]
-  (let [project (get-project* "lein-ui")]
-    (swap! (-> project ::run-state)
-           assoc :figwheel (figwheel-process project))))
+(defn start-figwheel! []
+  (swap! (-> (get-project*) ::run-state)
+         assoc :figwheel (figwheel-process)))
 
 ;;; Util
-(defn base-url []
-  (str "http://" "localhost" ":8000/"))
+(defn base-api-url []
+  (str "http://" "localhost" ":8000/api/project"))
 
 (defn url-for [path]
-  (str (base-url) path))
+  (str (base-api-url) path))
 
 
 ;;; API
-(defn url-for-project [name]
-  (url-for (str "api/projects/" name)))
 
-
-(defn get-repl-data [name]
-  (let [process (-> name get-project* ::run-state deref :repl)
-        url (str (url-for-project name) "/repl")]
+(defn get-repl-data []
+  (let [process (-> (get-project*) ::run-state deref :repl)
+        url (url-for "/repl")]
     (merge
      {:url url}
      (if process
        {:state :started
         :host (:host process)
         :port (:port process)
-        :stop-url (str (url-for-project name) "/repl/stop")
-        :eval-url (str (url-for-project name) "/repl/eval")}
+        :stop-url (url-for "/repl/stop")
+        :eval-url (url-for "/repl/eval")}
        {:state :stopped
-        :start-url (str (url-for-project name) "/repl/start")}))))
+        :start-url (url-for "/repl/start")}))))
 
 
 
@@ -199,33 +108,25 @@
     ::run-state
     ::raw})
 
-(defn get-readable-project [project-name]
-  (apply dissoc (get-project* project-name)
+(defn get-readable-project []
+  (apply dissoc (get-project*)
          non-data-keys))
 
-(defn get-readable-raw-project [project-name]
-  (apply dissoc (::raw (get-project* project-name))
+(defn get-readable-raw-project []
+  (apply dissoc (::raw (get-project*))
          non-data-keys))
 
-(defn project-summary [project]
-  {:url (url-for-project (:name project))
-   :name (:name project)
-   :version (:version project)
-   :root (:root project)})
-
-(defn get-projects []
-  {:projects (for [[name project] @projects]
-               (project-summary project))})
-
-(defn add-project [project-root]
-  (load-project! project-root))
-
+(defn project-summary []
+  (let [project (get-project*)]
+    {:url (base-api-url)
+     :name (:name project)
+     :version (:version project)
+     :root (:root project)}))
 
 (defn bootstrap-self []
   (load-project! ".")
-  (swap! (-> "lein-ui" (get-project*) ::run-state)
-         assoc :repl self-repl
-               :figwheel (figwheel-process (get-project* "lein-ui"))))
+  (swap! (-> (get-project*) ::run-state) :repl self-repl)
+  (start-figwheel!))
 
 
 ;;; Server
@@ -235,7 +136,7 @@
   (go-loop []
     (<! (timeout 500))
     (doseq [[project subscribers] @figwheel-receivers]
-      (let [output (-> (get-project* project)
+      (let [output (-> (get-project*)
                        ::run-state
                        deref
                        :figwheel
@@ -250,42 +151,25 @@
        (server/with-channel request channel
          (swap! figwheel-receivers assoc "lein-ui" channel)
          (server/on-close channel (fn [status] (swap! figwheel-receivers dissoc "lein-ui" channel)))))
-  (GET "/api/projects" []
-       {:body (pprint-str (get-projects))
-        :status 200})
-  (GET "/api/projects/:project-name" [project-name]
-       (let [project (get-project* project-name)
+  (GET "/api/project" []
+       (let [project (get-project*)
              run-state @(project ::run-state)]
          {:body (pprint-str
                  (merge (project-summary project)
                         ;; TODO just include these in the summary?
-                        {:map-url (str (url-for-project project-name)
-                                                "/map")
-                         :raw-map-url (str (url-for-project project-name)
-                                           "/raw-map")
-                         :repl (get-repl-data project-name)}))
+                        {:map-url (url-for "/map")
+                         :raw-map-url (url-for "/raw-map")
+                         :repl (get-repl-data)}))
           :status 200}))
-  (GET "/api/projects/:project-name/map" [project-name]
-       {:body (pprint-str (get-readable-project project-name))
+  (GET "/api/project/map" []
+       {:body (pprint-str (get-readable-project))
         :status 200})
-  (GET "/api/projects/:project-name/raw-map" [project-name]
-       {:body (pprint-str (get-readable-raw-project project-name))
+  (GET "/api/project/raw-map" []
+       {:body (pprint-str (get-readable-raw-project))
         :status 200})
-  (POST "/api/projects/:project-name/repl/start" [project-name]
-        (start-repl! project-name)
-        {:body (pprint-str (get-repl-data project-name))
-         :status 201})
-  (POST "/api/projects/:project-name/repl/eval" {{:keys [project-name
-                                                         code]} :params}
-        {:body (pprint-str (repl-eval! project-name code))
+  (POST "/api/project/repl/eval" {{:keys [code]} :params}
+        {:body (pprint-str (repl-eval! code))
          :status 200})
-  (POST "/api/projects" [root]
-        (let [name (load-project! root)]
-          {:status 201
-           :body (pprint-str (project-summary (get-project* name)))}))
-  (DELETE "/api/projects/:project-name" [project-name]
-          (unload-project! project-name)
-          {:status 204})
   (route/resources "/")
   (route/not-found "<p>Page not found.</p>"))
 
